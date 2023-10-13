56d6c00c3b09bc63da5df4bd7aab5b97c2babf61
Wulan013 <113662181+Wulan013@users.noreply.github.com>
2023-10-13 08:20:49 +0700

    Add files via upload

---
+++ uts.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package uts;
+
+/**
+ *
+ * @author 62813
+ */
+
+public static void main(String[] args) {   
+    public class uts{
+    String penjumlahan,pengurangan,perkalian,pembagian,pemangkatan ;
+        
+    
+    void Awalan (){
+    System.out.println("\n=========================");
+    System.out.println("        pilih              ");
+    System.out.println("    gunakan                  ");
+    System.out.println("        kalkulator            ");
+    System.out.println("=========================  ");
+    
+    System.out.println("penjumlahan  : "+this.penjumlahan);
+    System.out.println("pengurangan: "+this.pengurangan);
+    System.out.println("perkalian : "+this.perkalian);
+    System.out.println("pembagian : "+this.pembagian);
+    System.out.println("pemangkatan : "+this.pemangkatan);
+}
+}
+
+
+
+    
+        
+    
+        
+    
+
+   
+    
+    
